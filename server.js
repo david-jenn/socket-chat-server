7d008d187fb5985166c75952f101a04bac1a043f
David Jenn <david_jenn@insideranken.org>
2022-04-14 00:03:01 -0500

    commit

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server,{
   cors: {
-    origin: ['https://talk-rooms-david-jenn.herokuapp.com']
+    origin: ['http://localhost:3000', 'https://talk-rooms-david-jenn.herokuapp.com'],
+    method: ['GET', 'POST'],
   },
 });
 
