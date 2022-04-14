ddcccb576490f7bc3feb56aff04732b038676842
David Jenn <david_jenn@insideranken.org>
2022-04-14 00:33:42 -0500

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const server = http.createServer(app);
 const io = require('socket.io')(server,{
-  cors: {origin: "*"}
+  cors: {origin: "https://localhost:3000"}
    
 });
 
