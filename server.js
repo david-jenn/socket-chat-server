788d0d833eb091e32f4c590c81daf09139761e24
David Jenn <david_jenn@insideranken.org>
2022-04-14 23:10:41 -0500

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server,{
   cors: {
-    origin: ['https://talk-rooms-david-jenn.herokuapp.com/']
+    origin: ['https://talk-rooms-david-jenn.herokuapp.com']
   },
 });
 
