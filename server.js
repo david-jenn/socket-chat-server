be3ed1ce7069fd2871faeda642cb7529322c65f5
David Jenn <david_jenn@insideranken.org>
2022-10-11 23:11:39 -0500

    cors update

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const server = app.listen(port, () => {
 const io = require('socket.io')(server,{
   pingTimeout: 6000,
   cors: {
-    origin: 'http://localhost:3000', //https://talk-rooms-david-jenn.herokuapp.com http://localhost:3000
+    origin: 'https://talk-rooms-david-jenn.herokuapp.com', //https://talk-rooms-david-jenn.herokuapp.com http://localhost:3000
     credentials: true,
    },
 });
